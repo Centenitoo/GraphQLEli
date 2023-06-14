c25f9534c89ce34321e7b5367ca77c7c1a4805c4
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-13 20:54:04 -0600

    cambios

--- flores/schema.py
+++ flores/schema.py
@@ -5,7 +5,7 @@ import graphql_jwt
 import myapp.schema
 
 
-class Query(myapp.schema.Quey, users.schema.Query,flowers.schema.Query, graphene.ObjectType) :
+class Query(myapp.schema.Query, users.schema.Query,flowers.schema.Query, graphene.ObjectType) :
     pass
 
 class Mutation(myapp.schema.Mutation,users.schema.Mutation, flowers.schema.Mutation, graphene.ObjectType):
